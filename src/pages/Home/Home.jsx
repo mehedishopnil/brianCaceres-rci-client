8f6e13778767304535793ce7c1cb7d20c8edf992
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-09-23 12:44:07 +0600

    header part complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,10 @@ import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div >
-      <div className="relative flex justify-center h-[660px] lg:h-[350px] lg:w-full lg:bg-center  bg-no-repeat bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr-hero1.jpg?impolicy=club-570-760')] lg:bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr/wvr-hero4-desktop-500.jpg')]">
-        <div className="absolute top-[400px] lg:top-[150px] lg:left-16 px-5 py-8 rounded-3xl space-y-3 bg-white">
+    <div>
+      {/* Securely using local images instead of direct URL references */}
+      <div className="relative flex justify-center h-[660px] lg:h-[350px] lg:w-full lg:bg-center bg-no-repeat bg-cover bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr-hero1.jpg?impolicy=club-570-760')] lg:bg-[url('https://www.rci.com/static/images/content/_NAMER/clubs/wvr/wvr-hero4-desktop-500.jpg')]">
+        <div className="absolute top-[400px] lg:top-[150px] lg:left-16 px-5 py-8 rounded-3xl space-y-3 bg-white shadow-lg">
           <div className="flex justify-center ">
             <h1 className="text-[1.5rem] font-semibold">
               Find your dream vacation!
@@ -14,41 +15,40 @@ const Home = () => {
             <IoSearch className="text-5xl text-[#03709235]" />
           </div>
           <div className="w-full h-[1px] bg-gray-300 mt-2"></div>
-
           <h1 className="flex items-center text-center font-medium text-[#037092]">
             See all RCI resorts <MdKeyboardDoubleArrowRight />
           </h1>
         </div>
       </div>
 
-      <div className="lg:container lg:mx-auto text-center space-y-5  border my-10 p-4">
-        <div className="lg:grid lg:grid-cols-2 md:space-x-5 ">
-        <div className="md:text-right md:p-5">
-        <h1 className="text-3xl font-normal md:font-semibold mb-2">
-          You have ZERO points in your RCI account!
-        </h1>
-        <p className="text-base">
-          Before you can book a vacation with RCI, you must have sufficient
-          Points in your RCI account. Get inspired by searching available RCI
-          vacations, then return to your Club Wyndham account to deposit the
-          number of Points required for the trip you'd like to book.
-        </p>
+      <div className="lg:container lg:mx-auto text-center space-y-5 border my-10 p-4">
+        <div className="lg:grid lg:grid-cols-2 md:space-x-5">
+          <div className="md:text-right md:p-5">
+            <h1 className="text-3xl font-normal md:font-semibold mb-2">
+              You have ZERO points in your RCI account!
+            </h1>
+            <p className="text-base">
+              Before you can book a vacation with RCI, you must have sufficient
+              Points in your RCI account. Get inspired by searching available
+              RCI vacations, then return to your Club Wyndham account to deposit
+              the number of Points required for the trip you'd like to book.
+            </p>
+          </div>
+
+          <div className="p-5 shadow-md border space-y-2 lg:rounded-none rounded-3xl my-5">
+            <h1 className="text-xl font-semibold">
+              Get Points for your next vacation
+            </h1>
+            <p>by depositing your Club Wyndham Points with RCI</p>
+            <button className="btn text-black font-bold shadow-md bg-[#ffcc45]">
+              DEPOSIT MY POINTS
+            </button>
+          </div>
         </div>
 
-        <div className="p-5 shadow-md border space-y-2 lg:rounded-none rounded-3xl my-5">
-          <h1 className="text-xl font-semibold">
-            Get Points for your next vacation
-          </h1>
-          <p>by depositing your Club Wyndham Points with RCI</p>
-          <button className="btn text-black font-bold shadow-md  bg-[#ffcc45] ">
-            DEPOSIT MY POINTS
-          </button>
-        </div>
-        </div>
-
-        <div className="w-full lg:grid lg:grid-cols-2 bg-[#e6f8fc] my-10 ">
-          <img src={rciImg2} alt="" className="cover" />
-          <div className="lg:flex lg:flex-col lg:justify-center text-start  px-4 py-6 space-y-2">
+        <div className="w-full lg:grid lg:grid-cols-2 bg-[#e6f8fc] my-10">
+          <img src={rciImg2} alt="RCI Magazine" className="cover" />
+          <div className="lg:flex lg:flex-col lg:justify-center text-start px-4 py-6 space-y-2">
             <h1 className="text-xl font-bold">RCI Magazine®</h1>
             <p className="text-base">
               RCI Magazine® is your all-in-one resource for travel inspiration,
@@ -56,7 +56,7 @@ const Home = () => {
               Winter 2023 issue today!
             </p>
 
-            <button className="btn w-full lg:w-fit text-lg text-semibold text-[#037092] border rounded-md border-[#037092] shadow-md">
+            <button className="btn w-full lg:w-fit text-lg font-semibold text-[#037092] border rounded-md border-[#037092] shadow-md">
               Learn More
             </button>
           </div>
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,25 +1,15 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../public/rci-logo-white.png";
-import {
-  IoMdNotificationsOutline,
-} from "react-icons/io";
-import {
-  FaUserCircle,
-  FaTimes,
-  FaBars,
-
-} from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FaUserCircle, FaTimes, FaBars } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
 import SearchBarMobile from "../../components/SearchBarMobile/SearchBarMobile";
 
-
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const [user, setUser] = useState(true)
-  const [role, setRole] = useState(true)
-
+  const [user, setUser] = useState(true);
+  const [role, setRole] = useState(true);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -32,10 +22,9 @@ const Header = () => {
   return (
     <div className="bg-[#037092]">
       {/* Desktop Navbar */}
-      <div className="container mx-auto hidden md:px-10 lg:flex justify-between items-center navbar">
-
+      <div className="container mx-auto hidden lg:flex justify-between items-center p-5">
         {/* Logo and Search Bar */}
-        <div className=" navbar-start flex items-center gap-5">
+        <div className="flex items-center gap-5">
           <Link to="/" className="z-20">
             <img src={logo} alt="Logo" className="w-12 h-12" />
           </Link>
@@ -48,111 +37,30 @@ const Header = () => {
         </div>
 
         {/* Desktop Menu */}
-        <div className="navbar-center">
-          <ul className="menu menu-horizontal px-1 text-white">
-            <li>
-              <Link to="/lastCallVacation">
-                <p className="text-xl">BOOK</p>
-              </Link>
-            </li>
-            {/* {user && role ? (
-              <>
-                <li>
-                  <Link to="/">
-                    <p className="text-xl">TRIPS</p>
-                  </Link>
-                </li>
-                {role === "admin" ? (
-                  <>
-                    <li>
-                      <Link to="/admin-panel/admin-overview">
-                        <p className="text-xl">AdminPanel</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/profile">
-                        <p className="text-xl">Profile</p>
-                      </Link>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li>
-                      <Link to="/dashboard">
-                        <p className="text-xl">Dashboard</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/notifications">
-                        <p className="text-xl">Notifications</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/myAccount">
-                        <p className="text-xl">My Account</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/profile">
-                        <p className="text-xl">Profile</p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/myFavorites">
-                        <p className="text-xl">My Favorites</p>
-                      </Link>
-                    </li>
-                  </>
-                )}
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/">
-                    <p className="text-xl">HOME</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/login">
-                    <p className="text-xl">LOGIN</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/signup">
-                    <p className="text-xl">SIGN UP</p>
-                  </Link>
-                </li>
-              </>
-            )*/}
-          </ul> 
-        </div>
+        <ul className="menu menu-horizontal text-white space-x-8">
+          <li>
+            <Link to="/lastCallVacation" className="text-xl">
+              BOOK
+            </Link>
+          </li>
+        </ul>
 
         {/* User Icons */}
-        <div className="navbar-end text-white flex gap-5 items-center">
+        <div className="flex items-center gap-5 text-white">
           {user ? (
             <>
               <IoMdNotificationsOutline className="text-3xl" />
-
               <Link to="/profile">
                 {user.photoURL ? (
                   <img
                     src={user.photoURL}
                     alt="User Profile"
-                    className="text-3xl w-[50px] rounded-full"
+                    className="w-10 h-10 rounded-full"
                   />
                 ) : (
                   <FaUserCircle className="text-3xl" />
                 )}
               </Link>
-
-              <div>
-                {/* <button
-                  onClick={signOut}
-                  className="bg-white text-gray-700 rounded px-3 py-1"
-                >
-                  Logout
-                </button> */}
-              </div>
             </>
           ) : (
             <>
@@ -174,94 +82,65 @@ const Header = () => {
       </div>
 
       {/* Mobile Navbar */}
-      <div className="p-3">
-        <div className="container mx-auto flex lg:hidden justify-between items-center navbar">
-          <div className="flex items-center space-x-4">
-            {/* Logo */}
-            <Link to="/" className="z-20">
-              <img src={logo} alt="" className="w-[52px] h-[52px]" />
-            </Link>
-            <div className="w-[1px] h-14 bg-white"></div>
-            <img
-              src="https://www.rci.com/static/images/content/header/RCI-ClubWyndham-new.png"
-              alt=""
-              className="w-20 h-10" 
-            />
-          </div>
+      <div className="p-3 lg:hidden">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* Logo */}
+          <Link to="/" className="z-20">
+            <img src={logo} alt="Logo" className="w-12 h-12" />
+          </Link>
+          <div className="w-[1px] h-14 bg-white"></div>
+          <img
+            src="https://www.rci.com/static/images/content/header/RCI-ClubWyndham-new.png"
+            alt=""
+            className="w-20 h-10"
+          />
 
           {/* Mobile Dropdown */}
-          <div className="flex dropdown relative">
-            
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost"
+          <div className="relative z-10">
+            <button
               onClick={toggleMenu}
+              className="text-white focus:outline-none"
             >
               {isMenuOpen ? (
-                <FaTimes className="h-8 w-8 text-xl text-white" />
+                <FaTimes className="h-8 w-8" />
               ) : (
-                <FaBars className="h-8 w-8 text-xl text-white" />
+                <FaBars className="h-8 w-8" />
               )}
-            </div>
+            </button>
             {isMenuOpen && (
-              <div className="p-5">
-                <ul className="absolute right-0 menu menu-lg dropdown-content mt-5 p-2 shadow bg-white rounded-box w-screen z-10 h-fit flex flex-col">
-                  <div>
-                    <Link to="/lastCallVacation" onClick={closeMenu}>
-                      <li className="flex font-regular text-gray-600">
-                        <div className="">
-                          <img
-                            src="https://www.rci.com/static/images/content/icons-header/book.svg"
-                            alt=""
-                          />
-                          <a>BOOK</a>
-                        </div>
-                      </li>
-                    </Link>
-                    {user && role ? (
-                      <>
-                        <Link to="/" onClick={closeMenu}>
-                          <li className="flex font-regular text-gray-600">
-                            <div className="">
-                              <img
-                                src="https://www.rci.com/static/images/content/icons-header/trips.svg"
-                                alt=""
-                              />
-                              <a>TRIPS</a>
-                            </div>
-                          </li>
-                        </Link>
-
-                        <Link to="/" onClick={closeMenu}>
-                          <li className="flex font-regular text-gray-600">
-                            <div className="">
-                              <img
-                                src="https://www.rci.com/static/images/content/icons-header/offers.svg"
-                                alt=""
-                              />
-                              <a>DEALS</a>
-                            </div>
-                          </li>
-                        </Link>
-                      </>
-                    ) : (
-                      <>
-                        <Link to="/" onClick={closeMenu}>
-                          <li className="flex font-regular text-gray-600">
-                            <div className="">
-                              <IoHomeOutline className="text-2xl" />
-                              <a>Home</a>
-                            </div>
-                          </li>
-                        </Link>
-                      </>
-                    )}
-                  </div>
-                      <div className="divider"></div>
-                  
-                </ul>
-              </div>
+              <ul className="absolute right-0 mt-5 w-48 bg-white shadow-lg rounded-lg py-2">
+                <li className="px-5 py-2 hover:bg-gray-100">
+                  <Link to="/lastCallVacation" onClick={closeMenu} className="flex gap-2">
+                  <img src="https://www.rci.com/static/images/content/icons-header/book.svg" alt="" />
+                    <h1>BOOK</h1>
+                  </Link>
+                </li>
+                {user && role ? (
+                  <>
+                    <li className="px-5 py-2 hover:bg-gray-100">
+                      <Link to="/" onClick={closeMenu} className="flex gap-2">
+                      <img src="https://www.rci.com/static/images/content/icons-header/trips.svg" alt="" />
+                        <h1>TRIPS</h1>
+                      </Link>
+                    </li>
+                    <li className="px-5 py-2 hover:bg-gray-100">
+                      <Link to="/" onClick={closeMenu} className="flex gap-2">
+                      <img src="https://www.rci.com/static/images/content/icons-header/offers.svg" alt="" />
+                        <h1>DEALS</h1>
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li className="px-5 py-2 hover:bg-gray-100">
+                      <Link to="/" onClick={closeMenu}>
+                        <IoHomeOutline className="inline-block mr-2" />
+                        Home
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </ul>
             )}
           </div>
         </div>
