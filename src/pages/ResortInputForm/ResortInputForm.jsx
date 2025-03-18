77fae50db5742fe9adf4b865fbb6fe7a3513f507
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2025-03-18 13:35:31 +0600

    new update for ratings

--- src/pages/ResortInputForm/ResortInputForm.jsx
+++ src/pages/ResortInputForm/ResortInputForm.jsx
@@ -375,12 +375,12 @@ const ResortInputForm = () => {
             className="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           >
             <option value="">Select the Rating</option>
-            <option value="3">5</option>
-            <option value="4">6</option>
-            <option value="5">7</option>
-            <option value="5">8</option>
-            <option value="5">9</option>
-            <option value="5">10</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
           </select>
         </div>
 
